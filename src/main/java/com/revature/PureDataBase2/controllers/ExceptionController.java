e20485b3a33ce8d3c77bccb020d96bf7da726bfc
Sebastian Shader <sebfumaster@aol.com>
2023-06-09 12:01:50 -0700

    change unauthorized exception status code to unathorized

--- src/main/java/com/revature/PureDataBase2/controllers/ExceptionController.java
+++ src/main/java/com/revature/PureDataBase2/controllers/ExceptionController.java
@@ -96,7 +96,7 @@ public class ExceptionController {
         Map<String, Object> map = new HashMap<>();
         map.put("timestamp", new Date(System.currentTimeMillis()));
         map.put("message", e.getMessage());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(map);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
     }
 
     @ExceptionHandler(InvalidFormatException.class)
